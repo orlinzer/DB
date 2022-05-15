a5e32427de45e38094116047dd0af4569a63e026
Or Linzer <orlinzer@gmail.com>
2022-05-15 16:59:59 +0300

    update

--- index.js
+++ index.js
@@ -37,11 +37,42 @@ app.post('/', urlencodedParser, (req, res) => {
   //   console.log(req.params);
   // }
 
+  let dataResult = '';
+  for (let index = 0; index < data.length; index++) {
+    dataResult += index + ' ' + data[index].username + '<br/>';
+  }
+
   // console.log(req.params);
   res.send('<form method="POST">' +
   '<input type="text" name="username" value="' + data[0]?.username + '" />' +
   '<input type="submit" value="Click Me!" />' +
-  '</form>');
+  '</form>' + dataResult);
+});
+
+app.post('/update', urlencodedParser, (req, res) => {
+  console.log('post');
+  console.log(req.body.username);
+  // console.log(body('username'));
+  data.push({ username: req.body.username });
+
+  // console.log(body('username').toDate());
+  const errors = validationResult(req);
+  // console.log(errors.formatter(''));
+
+  // if (!errors.isEmpty()) {
+  //   console.log(req.params);
+  // }
+
+  let dataResult = '';
+  for (let index = 0; index < data.length; index++) {
+    dataResult += index + ' ' + data[index].username + '<br/>';
+  }
+
+  // console.log(req.params);
+  res.send('<form method="POST">' +
+  '<input type="text" name="username" value="' + data[0]?.username + '" />' +
+  '<input type="submit" value="Click Me!" />' +
+  '</form>' + dataResult);
 });
 
 app.listen(port, () => {
