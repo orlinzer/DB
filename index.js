85bbcbf8ae14a525e1182b321ccb0a9329fcca2a
Or Linzer <orlinzer@gmail.com>
2022-05-22 16:19:43 +0300

    delete

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ let data = [];
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const render = () => {};
+
 // create application/x-www-form-urlencoded parser
 // var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -58,6 +60,33 @@ app.post('/update', (req, res) => {
   console.log(req.body.username);
   // console.log(body('username'));
   data.push({ username: req.body.username });
+
+  // console.log(body('username').toDate());
+  const errors = validationResult(req);
+  // console.log(errors.formatter(''));
+
+  // if (!errors.isEmpty()) {
+  //   console.log(req.params);
+  // }
+
+  let dataResult = '';
+  for (let index = 0; index < data.length; index++) {
+    dataResult += index + ' ' + data[index].username + '<br/>';
+  }
+
+  // console.log(req.params);
+  res.send('<form method="POST">' +
+  '<input type="text" name="username" value="' + data[0]?.username + '" />' +
+  '<input type="submit" value="Click Me!" />' +
+  '</form>' + dataResult);
+});
+
+// app.post('/update', urlencodedParser, (req, res) => {
+app.post('/delete', (req, res) => {
+  console.log('delete');
+  console.log(req.body.id);
+  // console.log(body('username'));
+  data.filter((value) => value.id !== req.body.id);
 
   // console.log(body('username').toDate());
   const errors = validationResult(req);
